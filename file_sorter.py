08d009d99eb3d20af0ca620f865358027043fcfe
Curtis Yang <curtiy1@uci.edu>
2025-07-30 22:04:21 -0700

    Altered file_sorter to be OS-agnostic

--- file_sorter.py
+++ file_sorter.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from pathlib import Path
 
-PATH_USED = ""
+PATH_USED = "C:\\Users\\curty\\Documents"
 
 
 FILETYPES = {"images": ['.jpg', '.jpeg', '.png', '.bmp', '.heic'],
@@ -13,36 +13,46 @@ FILETYPES = {"images": ['.jpg', '.jpeg', '.png', '.bmp', '.heic'],
              }
 
 def create_directory_subfolders(base_path):
+    print("Creating subdirectory folders.")
     for file_type in FILETYPES.keys():
-        potential_path = Path(base_path + "\\" + file_type)
+        potential_path = os.path.join(base_path, file_type)
         try:
             os.makedirs(potential_path)
         except FileExistsError as e:
             print("This folder already exists: " + str(potential_path))
     
 def identify_files(base_path):
+    print("\nNow identifying file types.")
     files_sorted = {}
+    invalid_types = []
     for file in os.listdir(base_path):
         full_path = os.path.join(base_path, file)
         if os.path.isfile(full_path):
+            file_ext = os.path.splitext(file)[1].lower()
+            found = False
             for category, file_types in FILETYPES.items():
-                for specific_type in file_types:
-                    if os.path.splitext(file)[1] in specific_type:
-                        files_sorted[file] = category
-                        print(f"Adding '{file}' to '{category}' folder")
-                    else:
-                        print(f"'{file}' is not within the '{category}' folder")
-                        break
+                if file_ext in file_types:
+                    files_sorted[file] = category
+                    print(f"Adding '{file}' to '{category}' folder")
+                    found = True
+                    break
+            if not found:
+                invalid_types.append(file)
         else:
             print("This is not a file: " + file)
+    print(f"Files that are invalid types: {invalid_types}")
     return files_sorted
 
 def move_files(dict_files, source_path):
+    print("\nNow moving files into sorted folders.")
+    counter = 0
     for file, category in dict_files.items():
-        original_path = Path(source_path + "\\" + file)
-        new_dest = Path(source_path + "\\" + category)
+        original_path = os.path.join(source_path, file)
+        new_dest = os.path.join(source_path, category)
         shutil.move(original_path, new_dest)
         print(f"Moved '{file}' to '{new_dest}")
+        counter += 1
+    print(f"Moved {counter} files")
 
 
 if __name__ == "__main__":
